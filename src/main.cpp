a8686c1c8f22b168faee159317bf42453f00aaf5
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-08 14:34:15 -0500

    Delete node's structure tests

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,6 @@ void menu() {
 
 int main(int argc, char const *argv[]) {
     menu();
-	Node** test = new Node*[3];
-	test[0] = new NodeTree<Character>('x');
-	test[1] = new NodeTree<Integer>(4);
-	test[0]->right = new NodeTree<Integer>(99);
-	test[0]->printSelf();
-	Solver m("lkasldj");
     cout << "\nPress enter to continue ..."; 
     cin.get(); 
     return EXIT_SUCCESS;
