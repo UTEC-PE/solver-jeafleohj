e532d6121324e28f315b3003752f4f83683fba13
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-22 11:51:51 -0500

    Added a Token class

---
+++ include/token.hpp
@@ -0,0 +1,30 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+enum class type {
+				 variable,plus, minus, multiplication, exponentiation,
+				 division, number, lpar, rpar, EMPTY, END
+};
+
+struct Token {
+	type kind;
+	std::string value;
+	Token(type t, std::string v)
+		: kind(t), value(v)
+	{};
+	Token(type t, char v)
+		: kind(t)
+	{
+		value = v;
+	};
+	Token(const Token &t)
+		: kind(t.kind), value(t.value)
+	{}
+	Token operator=(const Token &t){
+		kind = t.kind;
+		value = t.value;
+		return *this;
+	}
+};
+
+#endif
