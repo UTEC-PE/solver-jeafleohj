be2e1d35444e25bc4093b5b1849714c24034c4be
jeafleohj <jhony.angulo@utec.edu.pe>
2018-09-22 11:50:28 -0500

    Implementing an abstract node

---
+++ include/node.hpp
@@ -0,0 +1,17 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+struct Node{
+	Node(){
+		left = right = nullptr;
+	}
+	Node(Node* l, Node* r)
+		: left(l), right(r)
+	{};
+	virtual void killSelf()=0;
+	virtual void printSelf()=0;
+	virtual ~Node()=0;
+	Node* left, *right;
+};
+
+#endif
